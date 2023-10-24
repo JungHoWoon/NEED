1054ad943aef1a507ac2ab7b2ae1d455565eb774
HoWoon Jung <jhu940810@gmail.com>
2023-10-24 17:53:20 +0900

    Feat: header navbar implement 웹 사이트의 header영역에 설정된 페이지로 이동하는 쇼핑몰 이름과 navbar를 구현

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { BiListPlus, BiSolidCart, BiListUl } from 'react-icons/bi';
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <Link to='/'>
+        <h1>NEED</h1>
+      </Link>
+      <nav>
+        <Link to='/products'>
+          products
+          <BiListUl />
+        </Link>
+        <Link to='/carts'>
+          carts
+          <BiSolidCart />
+        </Link>
+        <Link to='/products/new'>
+          new
+          <BiListPlus />
+        </Link>
+        <button>Login</button>
+      </nav>
+    </header>
+  );
 }
