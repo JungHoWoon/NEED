e1a70213c99f4990e918f2ee6daf3c0969bccafa
HoWoon Jung <jhu940810@gmail.com>
2023-10-24 19:36:40 +0900

    Design: header design

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BiListPlus, BiSolidCart, BiListUl } from 'react-icons/bi';
+import styles from './Navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <header className={styles.header}>
+      <Link to='/' className={styles.name}>
+        <h1>NEED</h1>
+      </Link>
+      <nav className={styles.nav}>
+        <Link to='/products' className={styles.button}>
+          <BiListUl />
+        </Link>
+        <Link to='/carts' className={styles.button}>
+          <BiSolidCart />
+        </Link>
+        <Link to='/products/new' className={styles.button}>
+          <BiListPlus />
+        </Link>
+        <button className={styles.login}>Login</button>
+      </nav>
+    </header>
+  );
+}
