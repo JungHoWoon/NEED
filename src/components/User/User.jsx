deb84950cc41ef0f82372c0906c8b3a906a159f0
HoWoon Jung <jhu940810@gmail.com>
2023-10-28 16:42:00 +0900

    Design: google profile img dseign

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './User.module.css';
 
 export default function User({ user: { photoURL, displayName } }) {
   return (
     <div>
-      <img src={photoURL} alt={displayName} />
+      <img src={photoURL} alt={displayName} className={styles.img} />
     </div>
   );
 }
