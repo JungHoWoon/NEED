8a8ccb4502e80b3bc0b0a8045a05f1fa52067141
HoWoon Jung <jhu940810@gmail.com>
2023-10-30 18:15:56 +0900

    Feat: new product frame implement 제품 등록 페이지에 필요한 뼈대 구현

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,5 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function NewProduct() {
-  return <div>NewProduct</div>;
+  const [product, setProduct] = useState({});
+
+  const [file, setFile] = useState();
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    if (name === 'file') {
+      setFile(files && files[0]);
+      return;
+    }
+    setProduct((product) => ({ ...product, [name]: value }));
+  };
+
+  return (
+    <section>
+      {file && <img src={URL.createObjectURL(file)} alt='file' />}
+      <form>
+        <input
+          type='file'
+          accept='image/*'
+          name='file'
+          required
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='title'
+          value={product.title ?? ''}
+          required
+          placeholder='제품명을 입력하세요.'
+          onChange={handleChange}
+        />
+        <input
+          type='number'
+          name='price'
+          value={product.price ?? ''}
+          required
+          placeholder='가격을 입력하세요.'
+          min='0'
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='category'
+          value={product.category ?? ''}
+          required
+          placeholder='카테고리를 입력하세요.'
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='description'
+          value={product.description ?? ''}
+          required
+          placeholder='제품 설명을 입력하세요.'
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='option'
+          value={product.option ?? ''}
+          required
+          placeholder='상품 옵션은 (,)로 구분해 주세요'
+          onChange={handleChange}
+        />
+        <button>등록하기</button>
+      </form>
+    </section>
+  );
 }
